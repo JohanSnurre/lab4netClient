c2bfbcc844c303f59e0c3229b525d9530b3767c8
JohanSnurre <9904jola@gmail.com>
2020-10-10 12:06:09 +0200

    yes

--- lab4netclient_real/lab4netclient_real.cpp
+++ lab4netclient_real/lab4netclient_real.cpp
@@ -10,101 +10,144 @@
 #include <Ws2tcpip.h>
 #include <thread>
 #include <conio.h>
-#include "ClientHeader.h""
+#include <unordered_map>
+#include "ClientHeader.h"
 #pragma comment(lib, "Ws2_32.lib")
 
 
+
 #define SERVER "127.0.0.1"	//ip address of udp server
+#define GUISERVER "::1" //GUI adress
 #define BUFLEN 512	//Max length of buffer
 #define PORT 5000	//The port on which to listen for incoming data
 #define MAPSIZE 201
+/*std::unordered_map<int, Coordinate> field;
+std::unordered_map<int, Coordinate>::iterator iter;*/
 int field[MAPSIZE][MAPSIZE];
+int thisPlayerID;
 
 
 
 
 
-int * getPos(int playerID) {
-
-	for (int i = 0; i < MAPSIZE; i++) {
-		for (int j = 0; j < MAPSIZE; j++) {
-			if (field[i][j] == playerID) {
-				int a[] = {i,j};
-				return a;
-
+int * getPos(int ID) {
+	int pos[2];
+	for (int x = 0; x < MAPSIZE; x++) {
+		for (int y = 0; y < MAPSIZE; y++) {
+			if (field[x][y] == ID) {
+				pos[0] = x;
+				pos[1] = y;
+				return pos;
 			}
+
 
 		}
 
-	}
-	int a[] = { 0,0 };
-	return a;
 
+	}
+
+	/*iter = field.find(ID);
+	int pos[] = { iter->second.x, iter->second.y };*/
+	pos[0] = 500;
+	pos[1] = 500;
+	return pos;
+}
+
+void insertToField(int ID, int pos[]) {
+	int x = pos[0];
+	int y = pos[1];
+	if (x == 500 || y == 500) return;
+
+	field[x][y] = ID;
+	/*Coordinate coord;
+	coord.x = pos[0];
+	coord.y = pos[1];
+	field[ID] = coord;*/
+	return;
 }
 
 
-void sendMoveReq(SOCKET sock, int x, int y, char buf[], int playerID, int seqNum) {
+void sendMoveReq(SOCKET sock, int x, int y, int seqNum) {
 
-
-	int *currPos = getPos(playerID);
-
+	char sendBuf[BUFLEN];
+	int *currPos = getPos(thisPlayerID);
+	//std::cout << currPos[0] << " : " << currPos[1] << std::endl;
 	
-
-
-
-
 	MoveEvent moveMsg;
-	moveMsg.pos.x = currPos[0] + x - 100;
-	moveMsg.pos.y = currPos[1] + y - 100;
+	moveMsg.pos.x = x;
+	moveMsg.pos.y = y;
+	std::cout << currPos[0] << " : " << currPos[1] << std::endl;
 	moveMsg.event.head.type = Event;
-	moveMsg.event.head.id = playerID;
+	moveMsg.event.head.id = thisPlayerID;
 	moveMsg.event.type = Move;
 	moveMsg.event.head.length = sizeof(moveMsg);
 	moveMsg.event.head.seq_num = seqNum;
-	memcpy((void*)buf, (void*)&moveMsg, sizeof(moveMsg));
+	memcpy((void*)sendBuf, (void*)&moveMsg, sizeof(moveMsg));
 
-	int sent = send(sock, buf, sizeof(buf), 0);
+	int sent = send(sock, sendBuf, sizeof(sendBuf), 0);
 	
 
 }
 
 
-void sendInfo(SOCKET sock, char buf[], int seqNum, int playerID) {
+void sendInfo(SOCKET sock, int seqNum) {
 	
-	std::fill_n(buf, sizeof(buf), 0);
+	
 	
 
 	char action;
 	while (true) {
-		std::cout << "::>";
+		//std::cout << "::>" << std::endl;
 		action = _getch();
 
 		switch (action) {
 
 		case 'w':
 			std::cout << "w" << std::endl;
-			sendMoveReq(sock, 0, -1, buf, playerID, seqNum);
+			sendMoveReq(sock, 0, -1, seqNum);
 			seqNum++;
 			break;
 
 		case 'a':
 			std::cout << "a" << std::endl;
-			sendMoveReq(sock, -1, 0, buf, playerID, seqNum);
+			sendMoveReq(sock, -1, 0, seqNum);
 			seqNum++;
 			break;
 
 		case 's':
 			std::cout << "s" << std::endl;
-			sendMoveReq(sock, 0, 1, buf, playerID, seqNum);
+			sendMoveReq(sock, 0, 1, seqNum);
 			seqNum++;
 			break;
 
 		case 'd':
 			std::cout << "d" << std::endl;
-			sendMoveReq(sock, 1, 0, buf, playerID, seqNum);
+			sendMoveReq(sock, 1, 0, seqNum);
+			seqNum++;
+			break;
+		/*case 'W':    //speedhack
+			std::cout << "w" << std::endl;
+			sendMoveReq(sock, 0, -3, seqNum);
 			seqNum++;
 			break;
 
+		case 'A':
+			std::cout << "a" << std::endl;
+			sendMoveReq(sock, -3, 0, seqNum);
+			seqNum++;
+			break;
+
+		case 'S':
+			std::cout << "s" << std::endl;
+			sendMoveReq(sock, 0, 3, seqNum);
+			seqNum++;
+			break;
+
+		case 'D':
+			std::cout << "d" << std::endl;
+			sendMoveReq(sock, 3, 0, seqNum);
+			seqNum++;
+			break;*/
 
 		default:
 			break;
@@ -112,46 +155,139 @@ void sendInfo(SOCKET sock, char buf[], int seqNum, int playerID) {
 		}
 
 
-		std::fill_n(buf, sizeof(buf), 0);
-
 
 	}
 }
+void updateGUI(SOCKET sock, int ID, sockaddr_in6 GUI, std::string GUImsg) {
+	char buf[BUFLEN];
+	std::fill_n(buf, BUFLEN, 0);
+	memcpy((void*)buf, (void*)&GUImsg, sizeof(GUImsg));
+	int sendOK = sendto(sock, GUImsg.c_str(),GUImsg.size() , 0, (struct sockaddr*)&GUI, sizeof(GUI));
+	if (sendOK == SOCKET_ERROR) {
+		std::cout << "ERROR " << WSAGetLastError() << std::endl;
+	}
 
 
-void receiveInfo(SOCKET sock, char buf[], int seqNum) {
+}
+
+void receiveInfo(SOCKET serverSock, SOCKET GUISock, sockaddr_in6 GUI,  int seqNum) {
+	char recvBuf[BUFLEN];
 	int receive;
-	std::fill_n(buf, sizeof(buf), 0);
+	//std::fill_n(recvBuf, sizeof(recvBuf), 0);
 
 
+	
+	
+	
 
 	while (true) {
 
-		receive = recv(sock, buf, sizeof(buf), 0);
-		MsgHead* head = (MsgHead*)buf;
-		std::cout << head->type << " : " << head->id << " : " << head->length << " : " << head->seq_num << std::endl;
+		receive = recv(serverSock, recvBuf, BUFLEN, 0);
+		MsgHead* head = (MsgHead*)recvBuf;
+		//std::cout << head->type << " : " << head->id << " : " << head->length << " : " << head->seq_num << std::endl;
+		
+
+		if (head->type == Join) {
+			std::cout << "Join";
+			
+		}
+
+		if (head->type == Leave) {
+			std::cout << "Leave";
+
+		}
+
+		if (head->type == Event) {
+			std::cout << "Event";
+
+			if (head->type == Move) {
+				std::cout << "Move";
+
+			}
+
+
+		}
+
+		
 
 		if (head->type == Change) {
+			std::cout << "Change" << std::endl;
 			ChangeMsg* change = (ChangeMsg*)head;
+			NewPlayerPositionMsg* NPPmsg = (NewPlayerPositionMsg*)head;
 			if (change->type == NewPlayerPosition) {
-				NewPlayerPositionMsg* msg = (NewPlayerPositionMsg*)buf;
+				std::cout << "NewPlayerPosition" << std::endl;
+				std::cout << head->id << " : " << head->type << " : " << change->type << " : " << NPPmsg->pos.x << " : " << NPPmsg->pos.y << std::endl;
+
+				
 				int x, y;
 				
-				x = msg->pos.x + 100;
-				y = msg->pos.y + 100;
-				field[x][y] = msg->msg.head.id;
+				x = NPPmsg->pos.x;
+				y = NPPmsg->pos.y;
+				//std::cout << NPPmsg->pos.x << " : " << NPPmsg->pos.y << std::endl;
+				field[x][y] = NPPmsg->msg.head.id;
+				int ID = NPPmsg->msg.head.id;
+				std::string color;
+				switch (NPPmsg->msg.head.id) {
 
+				case 0:
+					color = "white";
+					break;
 
-				std::cout << field[x][y] << std::endl;
+				case 1:
+					color = "blue";
+					break;
+
+				case 2:
+					color = "green";
+					break;
+
+				case 3:
+					color = "red";
+					break;
+				case 4:
+					color = "yellow";
+					break;
+				case 5:
+					color = "magenta";
+					break;
+
+				case 6:
+					color = "orange";
+					break;
+				case 7:
+					color = "pink";
+					break;
+				case 8:
+					color = "cyan";
+					break;
+
+				}
+				std::string GUImsg;
+				GUImsg = std::to_string(x) + "," + std::to_string(y) + "," + color;
+				updateGUI(GUISock, ID, GUI, GUImsg);
+				
+				
+			}
+			if (change->type == NewPlayer) {
+				std::cout << "NewPlayer" << std::endl;
+				
+				//NewPlayerMsg* NPmsg = (NewPlayerMsg*)recvBuf;
+				
+			}
+
+			if (change->type == PlayerLeave) {
+				std::cout << "PlayerLeave" << std::endl;
 			}
 
 		}
-		std::fill_n(buf, sizeof(buf), 0);
+		//std::fill_n(recvBuf, sizeof(recvBuf), 0);
 	}
 
 
 
 }
+
+
 
 
 int main()
@@ -174,23 +310,23 @@ int main()
 
 
 	//create hint structure for GUI
-	sockaddr_in GUI;
-	GUI.sin_family = AF_INET;
-	GUI.sin_port = htons(5000);
-	inet_pton(AF_INET, "127.0.0.1", &GUI.sin_addr);
+	sockaddr_in6 GUI;
+	memset(&GUI, 0, sizeof(GUI));
+	GUI.sin6_family = AF_INET6;
+	GUI.sin6_port = htons(5000);
+	inet_pton(AF_INET6, GUISERVER, &GUI.sin6_addr);
 
 	//create hint structure for the server
 	sockaddr_in server;
 	server.sin_family = AF_INET;
 	server.sin_port = htons(49152);
-	inet_pton(AF_INET, "127.0.0.1", &server.sin_addr);
+	inet_pton(AF_INET, SERVER, &server.sin_addr);
 
-	std::cout << "Connection successful" << std::endl;
 	//create UDP socket for the GUI
-	SOCKET GUISock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-
+	SOCKET GUISock = socket(PF_INET6, SOCK_DGRAM, 0);
+	
 	//create TCP socket for the server
-	SOCKET serverSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	SOCKET serverSock = socket(AF_INET, SOCK_STREAM, 0);
 
 	//connect to server
 	int res = connect(serverSock, (SOCKADDR*)&server, sizeof(server));
@@ -203,27 +339,30 @@ int main()
 	std::cout << "Connection successful" << std::endl;
 
 	int seqNum = 0;
-	int playerID = 0;
+	
 
-
-	char buf[BUFLEN];
-	char buff[BUFLEN];
 
 	JoinMsg joinMsg;
 	joinMsg.head.id = 0;
+	joinMsg.head.seq_num = seqNum;
 	joinMsg.head.length = sizeof(joinMsg);
 	joinMsg.head.type = Join;
 	memcpy((void*)sendBuf, (void*)&joinMsg, sizeof(joinMsg));
-	int BytesSent = send(serverSock, sendBuf, sizeof(sendBuf), 0);
+
+
+	int BytesSent = send(serverSock, sendBuf, BUFLEN, 0);
+
+
 	seqNum = seqNum + 1;
-	int rec = recv(serverSock, recvBuf, BUFLEN, 0);
+	int receive = recv(serverSock, recvBuf, BUFLEN, 0);
+	MsgHead* head = (MsgHead*)recvBuf;
+	thisPlayerID = head->id;
+	std::cout << head->type << " : " << head->id << " : " << head->seq_num <<  std::endl;
 
 	
-	
-	
 
-	std::thread sendInfo(sendInfo, serverSock, sendBuf, seqNum, playerID);
-	std::thread receiveInfo(receiveInfo, serverSock, recvBuf, seqNum);
+	std::thread sendInfo(sendInfo, serverSock, seqNum);
+	std::thread receiveInfo(receiveInfo, serverSock, GUISock, GUI, seqNum);
 
 	while (true) {
 
